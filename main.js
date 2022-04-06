92876b09ae65bb4ba60de6abbaa120faeab9402e
Manas-2010 <91479346+Manas-2010@users.noreply.github.com>
2022-04-06 19:32:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+function preload() {
+    classifier = ml5.imageClassifier("DoodleNet");
+}
+
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function draw() {
+    strokeWeight(13);
+    stroke("black");
+    if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, result) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(result);
+        document.getElementById("label").innerHTML = "Label : " + result[0].label;
+        document.getElementById("confidence").innerHTML = "Confidence : " + Math.round(result[0].confidence * 100) + "%";
+        utterThis = new SpeechSynthesisUtterance(result[0].label);
+        synth.speak(utterThis);
+    }
+}
+
+function clearCanvas() {
+    background("white");
+    console.log("Cleared Canvas!")
+}
